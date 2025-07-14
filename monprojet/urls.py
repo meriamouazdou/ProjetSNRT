796427d33704b17bccad3c271254ff19cfd6b2f9
meriamouazdou <147715669+meriamouazdou@users.noreply.github.com>
2025-07-14 16:26:08 +0100

    Update urls.py

--- monprojet/urls.py
+++ monprojet/urls.py
@@ -1,27 +1,17 @@
-"""
-URL configuration for monprojet project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Importer la vue de redirection si elle est dans monprojet/views.py
+from . import views  # ou commenter si tu ne l’utilises pas ici
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('dashboard/', include('dashboard.urls')),
+    path("dashboard/", include("dashboard.urls")),
+    path("users/", include("users.urls")),
+    path("redirect/", views.redirect_after_login, name="redirect_after_login"),  # si cette vue est bien ici
 ]
 
+# Ajout pour les fichiers média
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
